7074f6dfb5e893788292129527e045dfec15b31f
jeanhdes33 <153944916+jeanhdes33@users.noreply.github.com>
2024-05-02 19:47:20 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,18 @@ const cors = require('cors');
 const { test, registerUser, loginUser, getProfile, getUser, updateUserScore, getUserRanking, getBestScore } = require('../controllers/authController');
 const { createQuestion, getQuestionsByCategoryAndSubCategory, getRandomFootballQuestion, getRandomTennisQuestion, getRandomBasketballQuestion, getRandomCinemaQuestion, getRandomHistoireQuestion, getRandomSciencesQuestion, getRandomArtsQuestion, getRandomCultureGeneraleQuestion, submitAnswer, getCorrectAnswer } = require('../controllers/questionsController');
 
-router.use(
-    cors({
-        credentials: true,
-        origin: 'https://quoiz.onrender.com'
-    })
-);
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (origin === 'https://quoiz.onrender.com' || origin === 'http://localhost:3000') {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+router.use(cors(corsOptions));
 
 router.get('/', test);
 router.post('/register', registerUser);
@@ -17,19 +23,19 @@ router.post('/login', loginUser);
 router.get('/profile', getProfile);
 router.get('/user', getUser);
 router.post('/update-score', updateUserScore);
-router.get('/user-ranking', getUserRanking); // Route pour récupérer le classement de l'utilisateur
-router.get('/best-score', getBestScore); // Route pour récupérer le meilleur score de tous les utilisateurs
+router.get('/user-ranking', getUserRanking);
+router.get('/best-score', getBestScore);
 
 router.post('/questions', createQuestion);
 router.get('/questions', getQuestionsByCategoryAndSubCategory);
 router.get('/questions/football/random', getRandomFootballQuestion);
 router.get('/questions/tennis/random', getRandomTennisQuestion);
 router.get('/questions/basketball/random', getRandomBasketballQuestion);
-router.get('/questions/cinema/random', getRandomCinemaQuestion); // Route pour obtenir des questions de cinéma aléatoires
-router.get('/questions/histoire/random', getRandomHistoireQuestion); // Route pour obtenir des questions d'histoire aléatoires
+router.get('/questions/cinema/random', getRandomCinemaQuestion);
+router.get('/questions/histoire/random', getRandomHistoireQuestion);
 router.get('/questions/science/random', getRandomSciencesQuestion);
 router.get('/questions/arts/random', getRandomArtsQuestion);
-router.get('/questions/culture-generale/random', getRandomCultureGeneraleQuestion);  // Route pour obtenir des questions de sciences aléatoires
+router.get('/questions/culture-generale/random', getRandomCultureGeneraleQuestion);
 router.post('/questions/:questionId/answers', submitAnswer);
 router.get('/questions/:questionId/correct-answer', getCorrectAnswer);
 
